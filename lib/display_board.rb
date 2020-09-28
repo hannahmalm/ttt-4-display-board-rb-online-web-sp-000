4a2c720926fb698f494e1bc6e18dd98b78cf00bd
Hannah Malm <hannahemalm616@gmail.com>
2020-09-28 23:42:34 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,33 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+# def display_board(board)
+
+# board = [" "," "," "," "," "," "," "," "," "]
+
+# end
+
+def display_board(board)
+   board = ["","","","","","","","",""]
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+def display_board(board)
+board = ["","","","","","","","",""]
+  puts "   |   |   "
+  puts "-----------"
+  puts "   | X |   "
+  puts "-----------"
+  puts "   |   |   "
+end  
+  
+   board = ["","","","","","","","",""]
+  puts " O |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
